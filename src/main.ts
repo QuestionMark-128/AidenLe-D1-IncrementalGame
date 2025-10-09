bacaa0727e5ff44e6d37fd4246cef1cbcebbe83b
QuestionMark-128 <aiqle@ucsc.edu>
2025-10-09 23:27:19 +0000

    Changed the code to make the button easier to edit and add changes to it. Also added an emoji for the theme of the game

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import "./style.css";
 
-document.body.innerHTML = `
-  <button>click</button>
-`;
+const button = document.createElement("button");
+
+button.innerHTML = "💎";
+
+button.style.fontSize = "64px";
+button.style.borderRadius = "50%";
+button.style.width = "128px";
+button.style.height = "128px";
+button.style.display = "flex";
+button.style.alignItems = "center";
+button.style.justifyContent = "center";
+
+document.body.append(button);
