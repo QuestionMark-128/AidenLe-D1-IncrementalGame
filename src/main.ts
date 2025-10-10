8d1c0de69949f036137557f0cae9e5d6b5b27415
QuestionMark-128 <aiqle@ucsc.edu>
2025-10-10 01:10:41 +0000

    no longer starts off with automatic clicker. added button that costs 10 gems and can be bought to unlock automatic clicker. I also organized the entire code so its not confusing and renamed a variable to make more sense.

--- src/main.ts
+++ src/main.ts
@@ -9,43 +9,61 @@ button.style.height = "128px";
 button.style.display = "flex";
 button.style.alignItems = "center";
 button.style.justifyContent = "center";
+button.style.cursor = "pointer";
 
-button.innerHTML = "💎";
+button.textContent = "💎";
 
 //counter
 const counter = document.createElement("div");
 
 counter.style.fontSize = "32px";
 
+//upgrade button
+const upgrade1 = document.createElement("button");
+const upgrade1Cost = 10;
+
+upgrade1.disabled = true;
+
+upgrade1.textContent = "Miner: + 1gem/sec Cost: 10 Gems"
+
+//append elements
+document.body.append(counter, button, upgrade1);
+
+//gem variables
 let gemCount: number = 0;
+let delta = 0;
+
+//counter update and upgrade state
 function updateCount() {
   counter.textContent = `${gemCount.toFixed(1)} Gems`;
+  upgrade1.disabled = gemCount < upgrade1Cost;
 }
-updateCount();
 
+//gem button
 button.addEventListener("click", () => {
   gemCount += 1;
   updateCount();
 });
 
-//automatic clicking
-// setInterval(() => {
-//   gemCount += 1;
-//   updateCount();
-// }, 1000);
+//upgrade button
+upgrade1.addEventListener("click", () => {
+  if (gemCount >= upgrade1Cost) {
+    gemCount -= upgrade1Cost;
+    delta += 1;
+    updateCount();
+  }
+});
 
 //automatic clicking with continuous growth
 let zero = performance.now();
-const seconds = 1;
 
 function animate(time: number) {
   const timePassed = (time - zero) / 1000;
-  gemCount += seconds * timePassed;
+  gemCount += delta * timePassed;
   zero = time;
   updateCount();
   requestAnimationFrame(animate);
-}
-
+};
 requestAnimationFrame(animate);
 
-document.body.append(counter, button);
+updateCount();
